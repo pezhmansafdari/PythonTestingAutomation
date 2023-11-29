69a8e0ff3ec30f492cbe4464c99db1193511e448
Pezhman Baran (Safdari) <pezhman.safdary@gmail.com>
2023-11-29 10:21:43 +0200

    Update mathematicstests.py

--- mathematics_test.py
+++ mathematics_test.py
@@ -1,16 +1,36 @@
+<<<<<<< HEAD
 import unittest
-from math_functions import add, multiply, power
+import mathmatics
 
-class TestMathFunctions(unittest.TestCase):
+class TestMathMethods(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(5, 4), 9)
+        self.assertEqual(3, mathematics.add(1, 2))
+        self.assertEqual(10, mathematics.add(3, 7))
 
     def test_multiply(self):
-        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(50, mathematics.multiply(5, 10))
 
     def test_power(self):
-        self.assertEqual(power(2, 8), 256)
+        self.assertEqual(256, mathematics.power(2, 8))
 
 if __name__ == '__main__':
+=======
+import unittest
+import mathmatics
+
+class TestMathMethods(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(3, mathematics.add(1, 2))
+        self.assertEqual(10, mathematics.add(3, 7))
+
+    def test_multiply(self):
+        self.assertEqual(50, mathematics.multiply(5, 10))
+
+    def test_power(self):
+        self.assertEqual(256, mathematics.power(2, 8))
+
+if __name__ == '__main__':
+>>>>>>> 0e137f2d041d6b1d670bd01802e5f5c2b9dec48a
     unittest.main()
